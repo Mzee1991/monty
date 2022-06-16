d14b2c72ddedf3fd5da8b8884c4a2b3ae1cf5543
NikkyXO <alexisdame2017@gmail.com>
2022-06-16 11:42:27 +0100

    Fixed errors

--- get_functions.c
+++ get_functions.c
@@ -10,30 +10,30 @@
 void call_oper(stack_t **stack, char *oper, unsigned int line)
 {
 	int c;
-	instruction_t all_opers[] = {
-	{"push", instruct_push},
-	{"pall", instruct_pall},
-	{"pop", instruct_pop},
-	{"pint", instruct_pint},
-	{"swap", instruct_swap},
-	{"add", instruct_add},
-	{"sub", instruct_sub},
-	{"mul", instruct_mul},
-	{"div", instruct_div},
-	{"mod", instruct_mod},
-	{"nop", instruct_nop},
-	{"pchar", instruct_pchar},
-	{"pstr", instruct_pstr},
-	{"rotl", instruct_rotl},
-	{"rotr", instruct_rotr},
-	{"stack", set_stack},
-	{"queue", set_queue},
-	{NULL, NULL}
 
+	instruction_t all_opers[] = {
+		{"push", instruct_push},
+		{"pall", instruct_pall},
+		{"pop", instruct_pop},
+		{"pint", instruct_pint},
+		{"swap", instruct_swap},
+		{"add", instruct_add},
+		{"sub", instruct_sub},
+		{"mul", instruct_mul},
+		{"div", instruct_div},
+		{"mod", instruct_mod},
+		{"nop", instruct_nop},
+		{"pchar", instruct_pchar},
+		{"pstr", instruct_pstr},
+		{"rotl", instruct_rotl},
+		{"rotr", instruct_rotr},
+		{"stack", set_stack},
+		{"queue", set_queue},
+		{NULL, NULL}
 	};
 	for (c = 0; all_opers[c].opcode != NULL; c++)
 	{
-		if strcmp(oper, all_opers[c].opcode) == 0)
+		if (strcmp(oper, all_opers[c].opcode) == 0)
 		{
 			all_opers[c].f(stack, line);
 			return;
@@ -41,5 +41,5 @@ void call_oper(stack_t **stack, char *oper, unsigned int line)
 	}
 
 	fprintf(stderr, "L%u: unknown instruction %s\n", line, oper);
-	exit(EXIT_FAILURE)
+	exit(EXIT_FAILURE);
 }
